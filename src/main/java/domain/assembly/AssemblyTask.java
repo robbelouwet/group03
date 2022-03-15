ad909e7283ea82369c4834dd1885c1046a2f97a5
KarelSavonet <43341133+KarelSavonet@users.noreply.github.com>
2022-03-15 16:11:12 +0100

    added actions to the assemblytasks (not complete)

--- src/main/java/domain/assembly/AssemblyTask.java
+++ src/main/java/domain/assembly/AssemblyTask.java
@@ -7,10 +7,12 @@ import java.util.List;
 public class AssemblyTask {
     @Getter private boolean finished;
     @Getter private String name;
+    @Getter private List<String> actions;
 
-    public AssemblyTask(String name) {
+    public AssemblyTask(String name, List<String> actions) {
         this.name = name;
         this.finished = false;
+        this.actions = actions;
     }
 
     public static boolean isTaskName(String name, List<AssemblyTask> assemblyTasks) {
@@ -29,4 +31,6 @@ public class AssemblyTask {
     public String toString() {
         return String.format("Task %s: is %s", name, finished ? "finished" : "pending");
     }
+
+    public String getTaskInformation(){ return this.toString(); }
 }
--- src/main/java/persistence/WorkstationRepository.java
+++ src/main/java/persistence/WorkstationRepository.java
@@ -11,14 +11,46 @@ public class WorkstationRepository extends Repository<UUID, WorkStation> impleme
     public List<WorkStation> getWorkstations(){
         List<WorkStation> workStations = new ArrayList<>();
 
-        // TODO: mogen de assemblyTasks aangemaakt worden in deze repo?
-        AssemblyTask task1 = new AssemblyTask("Assembly car body");
-        AssemblyTask task2 = new AssemblyTask("Paint car");
-        AssemblyTask task3 = new AssemblyTask("Insert engine");
-        AssemblyTask task4 = new AssemblyTask("Insert gearbox");
-        AssemblyTask task5 = new AssemblyTask("Install seats");
-        AssemblyTask task6 = new AssemblyTask("Install airco");
-        AssemblyTask task7 = new AssemblyTask("Mount wheels");
+        // TODO: mogen de assemblyTasks aangemaakt worden in deze repo (en op deze manier lmao)?
+        List<String> actions1 = new ArrayList<>();
+        actions1.add("Lift the body shell onto the chassis frame.");
+        actions1.add("Bolt the shell and the frame together.");
+        AssemblyTask task1 = new AssemblyTask("Assembly car body", actions1);
+
+        List<String> actions2 = new ArrayList<>();
+        actions2.add("Make sure the car is clean, remove dust  if not.");
+        actions2.add("Sand the body.");
+        actions2.add("Clean it.");
+        actions2.add("Tape the surfaces.");
+        actions2.add("Paint the car.");
+        AssemblyTask task2 = new AssemblyTask("Paint car", actions2);
+
+        List<String> actions3 = new ArrayList<>();
+        actions3.add("Using the hoist, slowly lower the new motor into position.");
+        actions3.add("Connect it to the transmission.");
+        actions3.add("Reinstall the vacuum and fuel lines, and the throttle cables and accessories.");
+        actions3.add("Reinstall the air intake manifold, battery, and fan belts.");
+        actions3.add("Refill all of the fluids.");
+        AssemblyTask task3 = new AssemblyTask("Insert engine", actions3);
+
+        List<String> actions4 = new ArrayList<>();
+        actions4.add("Bolt the gearbox.");
+        actions4.add("Connect the starter.");
+        actions4.add("Connect the driveshaft.");
+        actions4.add("Refill the transmission fluid.");
+        actions4.add("Connect the exhaust.");
+        actions4.add("Connect the shifter.");
+        actions4.add("Connect the battery.");
+        AssemblyTask task4 = new AssemblyTask("Insert gearbox", actions4);
+
+        List<String> actions5 = new ArrayList<>();
+        AssemblyTask task5 = new AssemblyTask("Install seats", actions5);
+
+        List<String> actions6 = new ArrayList<>();
+        AssemblyTask task6 = new AssemblyTask("Install airco", actions6);
+
+        List<String> actions7 = new ArrayList<>();
+        AssemblyTask task7 = new AssemblyTask("Mount wheels", actions7);
 
         WorkStation ws1 = new WorkStation("Car Body Post",
                 new ArrayList<>(List.of(task1, task2)));
