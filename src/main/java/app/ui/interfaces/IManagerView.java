c6b434dc7ad3087f53ddd22d11cb39750d25bd55
AndresDeJonge <andres.dejonge@student.kdg.be>
2022-03-16 13:58:55 +0100

    forgot 1 todo to remove

--- src/main/java/app/ui/interfaces/IManagerView.java
+++ src/main/java/app/ui/interfaces/IManagerView.java
@@ -6,7 +6,6 @@ import java.util.Map;
 public interface IManagerView {
     void confirmMove(int timeSpent);
 
-    // TODO: pendingTasks & finishedTasks of type Map<String, List<String>>
     void showOverview(List<String> pendingOrders,
                       List<String> simFinishedOrders,
                       Map<String, List<String>> pendingTasks,
