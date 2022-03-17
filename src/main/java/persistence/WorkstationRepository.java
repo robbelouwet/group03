bb98700c068e4662a361eaec97d7cc17e778449e
Karel Savonet <savonetkarel@gmail.com>
2022-03-17 11:55:55 +0100

    removed redundant IWorkstationRepository

--- src/main/java/persistence/WorkstationRepository.java
+++ src/main/java/persistence/WorkstationRepository.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class WorkstationRepository extends Repository<UUID, WorkStation> implements IWorkstationRepository {
+public class WorkstationRepository extends Repository<UUID, WorkStation> {
     public List<WorkStation> getWorkstations(){
         List<WorkStation> workStations = new ArrayList<>();
 
